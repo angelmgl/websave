7c1149ebe4e0b01d48dea5f5fc6623d119911d91
Angel Mgl <angelemegeele@gmail.com>
2021-04-26 17:06:39 -0400

    Routes created and middlewares added

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const morgan = require("morgan");
 const exphbs = require("express-handlebars"); // HTML template engine
 const path = require("path"); // handle file paths in the project
 
+
 // inits
 const app = express();
+
 
 // settings
 app.set('port', process.env.PORT || 4000);
@@ -18,16 +20,26 @@ app.set('.hbs', exphbs({
 })); // set the template engine
 app.set('view engine', '.hbs');
 
+
 // middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); // to req and res json files through an API
+
 
 // global variables
-
+app.use((req, res, next) => {
+    next();
+});
 
 // routes
 app.use(require("./routes"));
+app.use(require("./routes/authentication"));
+app.use("/links", require("./routes/links"));
+
 
 // public files
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // starting the server
