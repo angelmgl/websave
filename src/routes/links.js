8290fca975420035777cce56e6500fa01363e765
Angel Mgl <angelemegeele@gmail.com>
2021-04-27 14:41:22 -0400

    Endpoint to modify links created

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,22 +4,22 @@ const router = express.Router();
 const db = require("../database");
 
 // get the form to create a new link
-router.get('/add', (req, res) => {
-    res.render('links/add');
+router.get("/add", (req, res) => {
+    res.render("links/add");
 });
 
 // post the data to create a new link
-router.post('/add', async (req, res) => {
+router.post("/add", async (req, res) => {
     const { title, url, description } = req.body;
     const newLink = {
         title,
         url,
-        description
+        description,
     };
 
     try {
-        await db.query('INSER INTO links set ?', [newLink]);
-        res.redirect('/links');
+        await db.query("INSER INTO links set ?", [newLink]);
+        res.redirect("/links");
     } catch (error) {
         console.log(error);
     }
@@ -30,17 +30,47 @@ router.get("/", async (req, res) => {
     try {
         const links = await db.query("SELECT * FROM links");
         res.render("links/list", { links });
-    } catch(error) {
+    } catch (error) {
         console.log(error);
     }
 });
 
-// delete a speceific link from the database
+// delete a specific link from the database
 router.get("/delete/:id", async (req, res) => {
     const { id } = req.params;
 
     try {
         await db.query("DELETE FROM links WHERE ID = ?", [id]);
+        res.redirect("/links");
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// render a form to modify data from a link
+router.get("/edit/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const link = await db.query("SELECT * FROM links WHERE ID = ?", [id]);
+        res.render("links/edit", { link: link[0] });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// save the changes on the link
+router.post("/edit/:id", async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = {
+        title,
+        description,
+        url,
+    };
+
+    try {
+        await db.query("UPDATE links set ? WHERE id = ?", [newLink, id]);
         res.redirect("/links");
     } catch (error) {
         console.log(error);
